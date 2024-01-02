619e8f43eeda29abfa480653335ac58f8baaaba6
Dekrazi <radoslaw.ziajka@gmail.com>
2024-01-02 17:36:04 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tkinter Timer App
+
+A simple timer application built using Tkinter in Python with sound playback using pygame.
+
+## Features
+
+- Set a timer in minutes.
+- Start and stop the timer.
+- Add preset time intervals (5 minutes, 10 minutes, 15 minutes).
+- Play a sound when the timer completes.
+
+## Requirements
+
+Make sure you have Python installed. Clone the repository and install the required packages using the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## To run the Timer
+```
+python timer.py
+
+```
+
--- timer.py
+++ timer.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 from pygame import mixer
 
+
 def start_timer():
     global timer_running
     timer_running = True
     input_value = int(input_box.get()) * 60  # Convert minutes to seconds
     update_timer(input_value)
+
 
 def update_timer(seconds):
     global timer_running
@@ -13,7 +15,7 @@ def update_timer(seconds):
         minutes, remaining_seconds = divmod(seconds, 60)
         label.config(text=f"Time left: {minutes} minutes {remaining_seconds} seconds")
         if seconds > 0:
-            window.after(1000, update_timer, seconds - 1)  
+            window.after(1000, update_timer, seconds - 1)
         else:
             label.config(text="Timer completed!")
             timer_running = False
@@ -28,15 +30,18 @@ def update_timer(seconds):
         else:
             label.config(text="Timer stopped")
 
+
 def stop_timer():
     global timer_running
     timer_running = False
     label.config(text="Timer stopped")
-    input_box.delete(0, tk.END) # clear the input box
+    input_box.delete(0, tk.END)  # clear the input box
+
 
 def add_minutes(value):
     input_box.delete(0, tk.END)
     input_box.insert(tk.END, value)
+
 
 def play_sound():
     mixer.init()
@@ -61,13 +66,19 @@ btn_start.grid(row=1, column=8, pady=10)
 btn_stop = tk.Button(master=window, text="Stop", command=stop_timer)
 btn_stop.grid(row=1, column=9, pady=10)
 
-btn_five = tk.Button(master=window, text="5 minutes", command=lambda: add_minutes(5), width=12)
+btn_five = tk.Button(
+    master=window, text="5 minutes", command=lambda: add_minutes(5), width=12
+)
 btn_five.grid(row=3, column=6, pady=10)
 
-btn_ten = tk.Button(master=window, text="10 minutes", command=lambda: add_minutes(10), width=12)
+btn_ten = tk.Button(
+    master=window, text="10 minutes", command=lambda: add_minutes(10), width=12
+)
 btn_ten.grid(row=3, column=7, pady=10)
 
-btn_fifteen = tk.Button(master=window, text="15 minutes", command=lambda: add_minutes(15), width=12)
+btn_fifteen = tk.Button(
+    master=window, text="15 minutes", command=lambda: add_minutes(15), width=12
+)
 btn_fifteen.grid(row=3, column=8, pady=10)
 
 window.mainloop()
